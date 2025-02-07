f79595a42887a1eef795fc90833bfdf0cee63e1e
margaretTere <may.terechenko@gmail.com>
2025-02-07 13:06:03 -0500

    Initial commit

---
+++ travel-app/travel-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-app
+//
+//  Created by Rita T on 2025-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ travel-app/travel-appTests/travel_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appTests.swift
+//  travel-appTests
+//
+//  Created by Rita T on 2025-02-07.
+//
+
+import Testing
+@testable import travel_app
+
+struct travel_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
